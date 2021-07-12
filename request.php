ff9721342cb6abc9dc1eee1cc1ba7a5772e4d116
TyranxX <ayan19961508@gmail.com>
2021-07-13 04:17:28 +0530

    modified API request

--- request.php
+++ request.php
@@ -1,7 +1,26 @@
 <?php
 
         $curl = curl_init();
-
+        
+        
+        $fileContent = base64_encode(file_get_contents($_FILES['files']['tmp_name']));
+        // $attach[]=array($_FILES['files']['name']=> "data:".$_FILES['files']['type']."".$fileContent);
+        
+        $mime = 'data:'.$_FILES['files']['type'].';';
+        $attach[]=array($_FILES['files']['name'] => $mime.'base64,'.$fileContent);
+        $data = array(
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'phone' => $_POST['mobile'],
+                'subject' => $_POST['topic'],
+                'topicId' => $_POST['topic'],
+                'message'=> $_POST['message'],
+                'attachments' => $attach
+            );
+        
+        $request = json_encode($data);
+        
+      
         curl_setopt_array($curl, array(
           CURLOPT_URL => 'https://lsfs.co.in/taskmanager/api/http.php/tickets.json',
           CURLOPT_RETURNTRANSFER => true,
@@ -11,13 +30,7 @@
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS =>'{"name": "John Doe",
-                "email": "johndoe@lorem.com",
-                "subject": "Lorem Ipsum",
-                "topicId": "1",
-                "message": "data:text/html,Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis sagittis turpis vel bibendum. Nunc eget tincidunt leo. Suspendisse a nibh vulputate, ultrices leo mollis, maximus nisl."
-
-                }',
+          CURLOPT_POSTFIELDS => $request,
           CURLOPT_HTTPHEADER => array(
             'X-API-Key: 80C1F5C66E2F0B00E756BCA8D46C6DA8',
             'Accept: application/json',
@@ -29,4 +42,6 @@
 
         curl_close($curl);
         echo $response;
+        // echo $request;
+        // echo $file;
 ?>
